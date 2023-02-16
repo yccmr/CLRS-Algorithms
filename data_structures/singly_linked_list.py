1b3f1998a0ed86c535a1dd52950f5a1b0a741ade
Yanze Song <yanzesong@rOot.local>
2023-02-16 00:42:31 -0700

    Drafted a few methods

--- data_structures/singly_linked_list.py
+++ data_structures/singly_linked_list.py
@@ -17,6 +17,9 @@ class Node:
     def set_next(self, new_next):
         self.__next = new_next
 
+    def __eq__(self, other):
+        return self.__data == other.get_data()
+
 
 class SLinkedList:
     def __init__(self):
@@ -29,15 +32,28 @@ class SLinkedList:
     def length(self):
         return self.__length
 
-    def is_found(self):
+    def search(self, target_node):
+        """ Return the index of the target_node. Return -1 if it doesn't exist """
+        pass
+
+    def add(self, new_node):
+        """ Add new_node at the head of the list """
+        pass
+
+    def append(self, new_node):
+        """ Append new_node to the end of the list """
         pass
 
     def insert(self, new_node, index):
-        """ Add new_node to the specified position of the list. If index >= length, add it at the end of the list """
+        """ Add new_node to the specified position of the list. If index >= length, append it at the end of the list """
         pass
 
     def remove(self, target_node):
         """ Remove the target node from the list, do nothing if it doesn't exist """
+        pass
+
+    def pop(self):
+        """ Remove the last element from the list """
         pass
 
     def clear(self):
@@ -47,6 +63,14 @@ class SLinkedList:
         print(self)
 
     def __str__(self):
-        pass
-
+        if self.is_empty():
+            return "(empty)"
+        else:
+            result = "head: "
+            current = self.__head
+            while current.next is not None:
+                result = result + str(current) + " -> "
+                current = current.next
+            result += str(current)
+            return result
 
