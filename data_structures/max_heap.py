a7c31138339562dfc3d15b9257936b7dbf998fd4
Yanze Song <yanzesong@rOot.local>
2023-02-18 22:35:31 -0700

    Drafted all methods

---
+++ data_structures/max_heap.py
@@ -0,0 +1,45 @@
+# Implementation of max heap
+
+def is_max_heap(arr):
+    pass
+
+
+class MaxHeap:
+    def __init__(self, arr):
+        self.__max_heap = arr
+        self.build_max_heap()
+
+    def __get_left_child(self, root_index):
+        pass
+
+    def __get_right_child(self, root_index):
+        pass
+
+    def __get_parent(self, root_index):
+        pass
+
+    def max_heapify(self):
+        pass
+
+    def build_max_heap(self):
+        pass
+
+    def heap_sort(self):
+        pass
+
+    def is_in_heap(self, target):
+        pass
+
+    def find_max(self):
+        pass
+
+    def extract_max(self):
+        pass
+
+    def insert(self, new):
+        pass
+
+    def delete(self, to_be_removed):
+        pass
+
+
