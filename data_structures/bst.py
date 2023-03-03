558588d62e9f1543f5bf76c0c1ed6d04c64e5438
Yanze Song <yanzesong@rOot.local>
2023-03-02 17:46:14 -0700

    Minimally tested

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -26,11 +26,11 @@ def general_get_max(root):
     if root.get_left() is None and root.get_right() is None:
         return root.get_data()
     elif root.get_left() is None and root.get_right() is not None:
-        return max(root.get_key(), general_get_max(root.get_right()))
+        return max(root.get_data(), general_get_max(root.get_right()))
     elif root.get_left() is not None and root.get_right() is None:
-        return max(root.get_key(), general_get_max(root.get_left()))
+        return max(root.get_data(), general_get_max(root.get_left()))
     else:
-        return max(root.get_key(), general_get_max(root.get_left()), general_get_max(root.get_right()))
+        return max(root.get_data(), general_get_max(root.get_left()), general_get_max(root.get_right()))
 
 
 def general_get_min(root):
@@ -40,11 +40,11 @@ def general_get_min(root):
     if root.get_left() is None and root.get_right() is None:
         return root.get_data()
     elif root.get_left() is None and root.get_right() is not None:
-        return min(root.get_key(), general_get_max(root.get_right()))
+        return min(root.get_data(), general_get_max(root.get_right()))
     elif root.get_left() is not None and root.get_right() is None:
-        return min(root.get_key(), general_get_max(root.get_left()))
+        return min(root.get_data(), general_get_max(root.get_left()))
     else:
-        return min(root.get_key(), general_get_max(root.get_left()), general_get_max(root.get_right()))
+        return min(root.get_data(), general_get_max(root.get_left()), general_get_max(root.get_right()))
 
 
 class BSTNode:
@@ -79,22 +79,24 @@ class BST:
     def get_root(self):
         return self.__root
 
+    def set_root(self, new_root):
+        self.__root = new_root
+
     def is_empty(self):
         return self.__root is None
 
     def is_found(self, root, target):
-        if self.is_empty():
+        if root is None:
             return False
+        elif root.get_data() == target:
+            return True
+        elif root.get_data() < target:
+            return self.is_found(root.get_right(), target)
         else:
-            if root.get_data() == target:
-                return True
-            elif root.get_data() < target:
-                return self.is_found(root.get_right(), target)
-            else:
-                return self.is_found(root.get_left(), target)
+            return self.is_found(root.get_left(), target)
 
     def get_max(self, root):
-        if self.is_empty():
+        if root is None:
             return None
         if root.get_right() is None:
             return root.get_data()
@@ -102,7 +104,7 @@ class BST:
             return self.get_max(root.get_right())
 
     def get_min(self, root):
-        if self.is_empty():
+        if root is None:
             return None
         if root.get_left() is None:
             return root.get_data()
@@ -111,10 +113,10 @@ class BST:
 
     def insert(self, root, new):
         """ Insert target into the BST (if it doesn't exist) s.t. the BST property is maintained """
-        if self.is_empty():
-            self.__root = BSTNode(new)
         if self.is_found(root, new):
             return
+        if root is None:
+            self.__root = BSTNode(new)
         else:
             if root.get_left() is None and new < root.get_data():
                 root.set_left(BSTNode(new))
@@ -127,13 +129,15 @@ class BST:
 
     def delete(self, root, target):
         """ Delete target from the BST (if it exists) s.t. the BST property is maintained, and return the new root """
-        if self.is_empty() or not self.is_found(root, target):
+        if root is None or not self.is_found(root, target):
             return root
         else:
-            if root.get_key() < target:
+            if root.get_data() < target:
                 root.set_right(self.delete(root.get_right(), target))
-            elif root.get_key() > target:
+                return root
+            elif root.get_data() > target:
                 root.set_left(self.delete(root.get_left(), target))
+                return root
             else:
                 if root.get_left() is None and root.get_right() is None:     # no child
                     return None
@@ -142,11 +146,21 @@ class BST:
                 elif root.get_left() is not None and root.get_right() is None:     # one left child
                     return root.get_left()
                 else:     # two children
-                    root.set_data(self.get_max(root.get_left()))
-                    root.set_left(self.delete(root.get_left(), target))
+                    left_max = self.get_max(root.get_left())
+                    root.set_data(left_max)
+                    root.set_left(self.delete(root.get_left(), left_max))
                     return root
 
-    def __str__(self):
-        pass
+    def inorder_string_repr(self, root):
+        if self.is_empty():
+            return "(empty)"
+        else:
+            if root is None:
+                return ''
+            else:
+                return self.inorder_string_repr(root.get_left()) + str(root.get_data()) + self.inorder_string_repr(root.get_right())
+
+    def __str__(self):     # should draw this as a tree...
+        return self.inorder_string_repr(self.__root)
 
 
