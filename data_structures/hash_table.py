53b1da90db0024607cad3ac76e831c32cf81dbe4
Yanze Song <yanzesong@rOot.local>
2023-03-02 22:50:22 -0700

    Basically finished, not tested

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -1,5 +1,4 @@
 # Implementation of hash table
-# Assume we're dealing with integers only
 
 class HashTable:
     def __init__(self, size=10):
@@ -10,15 +9,25 @@ class HashTable:
         return key % self.__size
 
     def is_found(self, target):
-        pass
+        hash_value = self.hash_map(target.get_key())      # get_key() may vary
+        return target in self.__table[hash_value]
 
+    # insert and delete is minimally implemented
     def insert(self, new):
-        pass
+        hash_value = self.hash_map(new.get_key())
+        self.__table[hash_value].append(new)
 
     def delete(self, target):
-        pass
+        hash_value = self.hash_map(target.get_key())
+        self.__table[hash_value].remove(target)
 
     def __str__(self):
-        pass
+        result = ''
+        for i in range(self.__size):
+            result += "[{}]: ".format(i)
+            for j in range(len(self.__table[i])-1):
+                result = result + str(self.__table[i][j]) + " -> "
+            result = result + str(self.__table[i][-1]) + '\n'
+        return result
 
 
